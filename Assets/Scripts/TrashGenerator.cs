401daabc93ad9e1dfdbabfc4d29eefe0da5272c3
magentaong <ongmagenta@gmail.com>
2026-09-20 12:55:29 +0800

    Replenish trash to maintain at least five pieces

--- Assets/Scripts/TrashGenerator.cs
+++ Assets/Scripts/TrashGenerator.cs
@@ -1,22 +1,35 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class TrashGenerator : MonoBehaviour 
+public class TrashGenerator : MonoBehaviour
 {
     public GameObject TrashPrefab;
     public Sprite[] TrashSprites;
+    [Min(5)] public int MinimumTrashCount = 5;
+
+    private readonly List<GameObject> activeTrash = new List<GameObject>();
+
     private void Start()
+    { ReplenishTrash(); }
+
+
+    private void Update()
     {
-        for(int i = 0; i < 5; i++)
-        {
-            SpawnTrash();
-        }
-        
+        activeTrash.RemoveAll(trash => trash == null);
+        ReplenishTrash();
+    }
+
+    private void ReplenishTrash()
+    {
+        while (activeTrash.Count < Mathf.Max(5, MinimumTrashCount))
+        { SpawnTrash(); }
     }
     // Spawn trash in random coordinate of 256x256 grid, randomise trash sprite
     private void SpawnTrash()
     {
         Vector3 TrashSpawnCoordinate = new Vector3(Random.Range(-128, 128), Random.Range(-128, 128), 0);
         GameObject NewTrash = Instantiate(TrashPrefab, TrashSpawnCoordinate, Quaternion.identity);
+        activeTrash.Add(NewTrash);
         var NewTrashSprite = TrashSprites[Random.Range(0, TrashSprites.Length)];
         NewTrash.transform.Find("trash").GetComponentInChildren<SpriteRenderer>().sprite = NewTrashSprite;
     }
